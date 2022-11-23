e08cbd3971ba8302413af76ecbe8b073e5f08db8
John <mktkamehouse@gmail.com>
2022-11-23 16:17:47 -0300

    end of the first day of code

---
+++ bandNamegenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to band's name generator!")
+
+city = input("Type the city you grew up in:")
+
+pet = input("Now, type a name of your pet:")
+
+print("Your band name is " + city + " " + pet)
